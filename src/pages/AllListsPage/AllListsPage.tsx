8b470fb5e7633d80eeb5b0eaa3f033a2427438ff
MTG\mtg09 <50504183+MTG2000@users.noreply.github.com>
2024-05-06 22:16:41 +0400

    fix build issue

--- src/pages/AllListsPage/AllListsPage.tsx
+++ src/pages/AllListsPage/AllListsPage.tsx
@@ -1,6 +1,5 @@
-import { createRoute } from "@/router/create-route";
 import { Navigate } from "react-router-dom";
 
 export default function AllListsPage() {
-  return <Navigate to={createRoute({ type: "homepage" })} />;
+  return <Navigate to={"/"} />;
 }
